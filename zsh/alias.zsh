d6f337970661d96fca196beb97f51ed4d7017565
Jeff Pollard <jeff.pollard@gmail.com>
2012-12-07 16:40:55 -0800

    Add go path

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -6,3 +6,4 @@ alias hgrep='history | grep'
 alias envthis='VIRTUAL_ENV=$(basename $(pwd)) && remkvirtualenv'
 alias mkvirtualenv='genv --unset=PYTHONDONTWRITEBYTECODE && \mkvirtualenv -p `which python`'
 alias remkvirtualenv='VENVBAK=`basename $VIRTUAL_ENV` && deactivate || true && rmvirtualenv $VENVBAK && mkvirtualenv -p `${BREW} --prefix python`/bin/python $VENVBAK && pip install notsetuptools && pip install -e .'
+/Users/jeff/Code/go/src
