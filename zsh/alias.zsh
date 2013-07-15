d130a24c952aaf49b8d8c297c4c6f7ecaad123d1
Jeff Pollard <jeff.pollard@gmail.com>
2013-07-15 09:12:57 -0700

    remove useless virtualenv stuff

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,6 +3,3 @@ alias disqus-vm-ssh='cd ~/Code/disqus-ops/vagrant/local/ && vagrant ssh'
 alias remove-pyc='find . -name "*.pyc" -exec rm -rf {} \;'
 alias hgrep='history | grep'
 
-alias envthis='VIRTUAL_ENV=$(basename $(pwd)) && remkvirtualenv'
-alias mkvirtualenv='genv --unset=PYTHONDONTWRITEBYTECODE && \mkvirtualenv -p `which python`'
-alias remkvirtualenv='VENVBAK=`basename $VIRTUAL_ENV` && deactivate || true && rmvirtualenv $VENVBAK && mkvirtualenv -p `${BREW} --prefix python`/bin/python $VENVBAK && pip install notsetuptools && pip install -e .'
