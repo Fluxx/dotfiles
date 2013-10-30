5bd522fc18df2128eceed6d094403f8fe06155d6
Jeff Pollard <jeff.pollard@gmail.com>
2013-10-29 17:54:17 -0700

    Alias cleanups

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -1,5 +1,3 @@
-source /usr/local/phabricator/arcanist/resources/shell/bash-completion
 eval "$(rbenv init -)"
 source /usr/local/bin/virtualenvwrapper.sh
-source "`brew --prefix grc`/etc/grc.bashrc"
 export GOPATH=/Users/jeff/Code/go
