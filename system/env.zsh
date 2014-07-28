7877d3e3689a783f63af6975104ace5f300715dc
Jeff Pollard <jeff.pollard@gmail.com>
2014-07-28 13:40:55 -0700

    Update zk path

--- system/env.zsh
+++ system/env.zsh
@@ -1,5 +1,5 @@
 export EDITOR='slime --wait'
 export KAFKA_PATH=/Users/jeff/Code/kafka-0.7.1-incubating
-export ZOOKEEPER_PATH=/usr/local/Cellar/zookeeper/3.4.3/libexec
+export ZOOKEEPER_PATH=/usr/local/Cellar/zookeeper/3.4.6/libexec
 export NOSE_REDNOSE=1
 export MACOSX_DEPLOYMENT_TARGET='10.7'
