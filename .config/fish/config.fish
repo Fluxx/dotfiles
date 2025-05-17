49a2296d48da0ce02fad145e2ce875736346a6f9
Jeff Pollard <jeff.pollard@gmail.com>
2025-05-17 13:52:19 -0700

    Cleanup the fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,13 @@
-alias paasage='USER=jeffpollard ~/Code/Strava/configuration/mesos/tools/paasage --no-connectivity-check'
 alias clipboard-restart='killall pboard'
+
+if test -f ~/.config/fish/private.fish
+    source ~/.config/fish/private.fish
+end
 
 fish_add_path /opt/Homebrew/bin
 fish_add_path /opt/bin
 fish_add_path /usr/local/bin
-fish_add_path ~/Code/Strava/configuration/mesos/tools/
+fish_add_path ~/.rbenv/shims/
+fish_add_path --append --path ~/Code/Strava/configuration/mesos/tools/
+
+uv generate-shell-completion fish | source
